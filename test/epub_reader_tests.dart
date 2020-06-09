8e3b306bd7b74ea858cace77a38c46a80745a03d
Marco Ivarsson <marco.ivarsson@gmail.com>
2020-06-09 14:06:24 +0200

    Group  epub reader tests

--- test/epub_reader_tests.dart
+++ test/epub_reader_tests.dart
@@ -7,71 +7,84 @@ import 'package:test/test.dart';
 
 import 'package:epub/epub.dart';
 
-main() async {
-  String fileName = "hittelOnGoldMines.epub";
-  String fullPath =
-      path.join(io.Directory.current.path, "test", "res", fileName);
-  var targetFile = new io.File(fullPath);
-  if (!(await targetFile.exists())) {
-    throw new Exception("Specified epub file not found: ${fullPath}");
-  }
+void main() async {
+  group('Hittel on Gold Mines', () {
+    var targetFile;
+    var bytes;
 
-  List<int> bytes = await targetFile.readAsBytes();
-  test("Test Epub Ref", () async {
-    EpubBookRef epubRef = await EpubReader.openBook(bytes);
+    setUpAll(() async {
+      var fileName = "hittelOnGoldMines.epub";
+      var fullPath =
+          path.join(io.Directory.current.path, "test", "res", fileName);
 
-    expect(epubRef.Author, equals("John S. Hittell"));
-    expect(epubRef.Title, equals("Hittel on Gold Mines and Mining"));
-  });
-  test("Test Epub Read", () async {
-    EpubBook epubRef = await EpubReader.readBook(bytes);
+      targetFile = new io.File(fullPath);
 
-    expect(epubRef.Author, equals("John S. Hittell"));
-    expect(epubRef.Title, equals("Hittel on Gold Mines and Mining"));
-  });
-
-  test("Test can read", () async {
-    String baseName =
-        path.join(io.Directory.current.path, "test", "res", "std");
-    io.Directory baseDir = new io.Directory(baseName);
-    if (!(await baseDir.exists())) {
-      throw new Exception("Base path does not exist: ${baseName}");
-    }
-
-    await baseDir
-        .list(recursive: false, followLinks: false)
-        .forEach((io.FileSystemEntity fe) async {
-      try {
-        io.File tf = new io.File(fe.path);
-        List<int> bytes = await tf.readAsBytes();
-        EpubBook book = await EpubReader.readBook(bytes);
-        expect(book, isNotNull);
-      } catch (e) {
-        print("File: ${fe.path}, Exception: ${e}");
-        fail("Caught error...");
+      if (!(targetFile.existsSync())) {
+        throw new Exception("Specified epub file not found: ${fullPath}");
       }
+
+      bytes = targetFile.readAsBytesSync();
+    });
+
+    test("Test Epub Ref", () async {
+      var epubRef = await EpubReader.openBook(bytes);
+
+      expect(epubRef.Author, equals("John S. Hittell"));
+      expect(epubRef.Title, equals("Hittel on Gold Mines and Mining"));
+    });
+
+    test("Test Epub Read", () async {
+      var epubRef = await EpubReader.readBook(bytes);
+
+      expect(epubRef.Author, equals("John S. Hittell"));
+      expect(epubRef.Title, equals("Hittel on Gold Mines and Mining"));
     });
   });
 
-  test("Test can open", () async {
-    var baseName = path.join(io.Directory.current.path, "test", "res", "std");
-    var baseDir = new io.Directory(baseName);
-    if (!(await baseDir.exists())) {
-      throw new Exception("Base path does not exist: ${baseName}");
-    }
+  group('All files', () async {
+    var baseDir;
 
-    await baseDir
-        .list(recursive: false, followLinks: false)
-        .forEach((io.FileSystemEntity fe) async {
-      try {
-        var tf = new io.File(fe.path);
-        var bytes = await tf.readAsBytes();
-        var ref = await EpubReader.openBook(bytes);
-        expect(ref, isNotNull);
-      } catch (e) {
-        print("File: ${fe.path}, Exception: ${e}");
-        fail("Caught error...");
+    setUpAll(() async {
+      var baseName =
+          path.join(io.Directory.current.path, "test", "res", "std");
+
+      baseDir = new io.Directory(baseName);
+
+      if (!(await baseDir.exists())) {
+        throw new Exception("Base path does not exist: ${baseName}");
       }
+    });
+
+    test("Test can read", () async {
+      await baseDir
+          .list(recursive: false, followLinks: false)
+          .forEach((io.FileSystemEntity fe) async {
+        try {
+          var tf = new io.File(fe.path);
+          List<int> bytes = await tf.readAsBytes();
+          var book = await EpubReader.readBook(bytes);
+          expect(book, isNotNull);
+        } catch (e) {
+          print("File: ${fe.path}, Exception: ${e}");
+          fail("Caught error...");
+        }
+      });
+    });
+
+    test("Test can open", () async {
+      await baseDir
+          .list(recursive: false, followLinks: false)
+          .forEach((io.FileSystemEntity fe) async {
+        try {
+          var tf = new io.File(fe.path);
+          var bytes = await tf.readAsBytes();
+          var ref = await EpubReader.openBook(bytes);
+          expect(ref, isNotNull);
+        } catch (e) {
+          print("File: ${fe.path}, Exception: ${e}");
+          fail("Caught error...");
+        }
+      });
     });
   });
 }
